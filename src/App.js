96be07d76319889939861fba434a5cbab604cff4
Greg VENET <gregvenet@gmail.com>
2023-03-17 14:30:02 +0100

    test gh-pages app.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import "./App.scss"
 import { Routes, Route } from "react-router-dom"
 import Layout from "./components/Layout"
 import Home from "./components/Home"
 import About from "./components/About"
 import Contact from "./components/Contact";
+import "./App.scss"
 
 function App() {
   return (
+    <>
       <Routes>
-        <Route path="/Portfolio" element={<Layout />}>
+        <Route path="/*" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
         </Route>
       </Routes>
+    </>
   )
 }
 
