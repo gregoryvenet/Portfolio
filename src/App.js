e85cb97821c273894ff9a3bc90791c5ff7e32c8a
Greg VENET <gregvenet@gmail.com>
2023-03-18 18:35:05 +0100

    modif routes sidebar

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import { Routes, Route } from "react-router-dom"
 import Layout from "./components/Layout"
 import Home from "./components/Home"
 import About from "./components/About"
-import Contact from "./components/Contact";
+import Contact from "./components/Contact"
 import "./App.scss"
 
 function App() {
   return (
-      <Routes>
-        <Route path="/*" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-        </Route>
-        </Routes>
+    <>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="*" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
+      </Routes>
+    </>
   )
 }
 
