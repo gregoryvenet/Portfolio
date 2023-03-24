46393b27a912a7e4388354cc37f7318a152ba059
Greg VENET <gregvenet@gmail.com>
2023-03-24 12:25:21 +0100

    replace fetch with axios

--- src/components/WeatherWidget/index.js
+++ src/components/WeatherWidget/index.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import { AlertGps } from "../Alert"
+import axios from "axios"
 import "./index.scss"
 
 import clearSkyDay from "../../assets/icons/01d.svg"
@@ -23,7 +24,8 @@ import freezingRainNight from "../../assets/icons/freezing rain n.svg"
 
 const WeatherWidget = () => {
   const apiKey = process.env.REACT_APP_WEATHER_API_KEY
-  const [ weatherData, setWeatherData ] = useState(null)
+  const [weatherData, setWeatherData] = useState(null)
+  console.log("weatherData: ", weatherData)
   const [location, setLocation] = useState(null)
 
   useEffect(() => {
@@ -43,19 +45,18 @@ const WeatherWidget = () => {
     const fetchWeatherData = async () => {
       if (location) {
         try {
-          const response = await fetch(
+          const response = await axios.get(
             `https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=${apiKey}&units=metric&lang=fr`
           )
-          if (!response.ok) {
+
+          if (!response) {
             throw new Error(
               "Erreur lors de la récupération des données météorologiques"
             )
           }
-          const data = await response.json()
-          setWeatherData(data)
+          setWeatherData(response.data)
         } catch (error) {
           AlertGps()
-          console.error(error)
         }
       }
     }
@@ -72,72 +73,80 @@ const WeatherWidget = () => {
       currentTime <= weatherData.sys.sunset
     //Orage 11j
     if (weatherId >= 200 && weatherId <= 232) {
-      return isDayTime ?
+      return isDayTime ? (
         <img src={thunderstormDay} alt="thunderstorm icon" />
-      :
+      ) : (
         <img src={thunderstormNight} alt="thunderstorm icon" />
+      )
 
       //Bruine 09j
     } else if (weatherId >= 300 && weatherId <= 321) {
       return <img src={showerRain} alt="shower rain icon" />
       //Pluie avec soleil/lune 10j
     } else if (weatherId >= 500 && weatherId <= 504) {
-      return isDayTime ?
+      return isDayTime ? (
         <img src={rainDay} alt="rain day icon" />
-       :
+      ) : (
         <img src={rainNight} alt="rain night icon" />
+      )
 
       //Pluie verglassante 13j
     } else if (weatherId === 511) {
-      return isDayTime ?
+      return isDayTime ? (
         <img src={freezingRainDay} alt="freezing rain day icon" />
-       :
+      ) : (
         <img src={freezingRainNight} alt="freezing rain night icon" />
+      )
 
       //Pluie gris total 09j
     } else if (weatherId >= 520 && weatherId <= 531) {
       return <img src={showerRain} alt="thunderstorm day icon" />
       //Neige 13j
     } else if (weatherId >= 600 && weatherId <= 622) {
-      return isDayTime ?
+      return isDayTime ? (
         <img src={snowDay} alt="snow day icon" />
-       :
+      ) : (
         <img src={snowNight} alt="snow night icon" />
+      )
 
       //Brume 50j
     } else if (weatherId >= 701 && weatherId <= 781) {
-      return isDayTime ?
+      return isDayTime ? (
         <img src={mistDay} alt="mist day icon" />
-       :
+      ) : (
         <img src={mistNight} alt="mist night icon" />
+      )
 
       //ciel clair 01dn
     } else if (weatherId === 800) {
-      return isDayTime ?
+      return isDayTime ? (
         <img src={clearSkyDay} alt="clear sky day icon" />
-       :
+      ) : (
         <img src={clearSkyNight} alt="clear sky night icon" />
+      )
 
       //Quelques Nuages 11-25 % 02dn
     } else if (weatherId === 801) {
-      return isDayTime ?
+      return isDayTime ? (
         <img src={fewCloudsDay} alt="few clouds day icon" />
-       :
+      ) : (
         <img src={fewCloudsNight} alt="few clouds night icon" />
+      )
 
       //nuages ​​dispersés : 25-50 % 03dn
     } else if (weatherId === 802) {
       return <img src={scatteredClouds} alt="scattered clouds icon" />
       //nuages ​​fragmentés : 51-84 % 04dn
     } else if (weatherId === 803 && weatherId === 804) {
-      return isDayTime ?
+      return isDayTime ? (
         <img src={brokenCloudsDay} alt="broken clouds day icon" />
-       :
+      ) : (
         <img src={brokenCloudsNight} alt="broken clouds night icon" />
-
-      } else{
-        return <p>Icone non disponible</p>}
-      }
+      )
+    } else {
+      return <p>Icone non disponible</p>
+    }
+  }
 
   return (
     <div className="weather-icon">
